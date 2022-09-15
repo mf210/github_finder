1dc764a417029edd306a60152b7762b1225ea694
Mehdi Faresi <mfaresi210@gmail.com>
2022-09-15 09:01:18 +0430

    Show alert message

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ searchInput.addEventListener('keyup', (e) => {
             .then(data => {
                 if(data.profile.message === 'Not Found'){
                     // Show alert
-
+                    ui.showAlert('User not found', 'alert alert-danger');
                 } else {
                     // Show profile
                     ui.showProfile(data.profile);
@@ -26,5 +26,6 @@ searchInput.addEventListener('keyup', (e) => {
             });
     } else {
         // Clear profile
+        ui.clearProfile();
     }
 });
--- ui.js
+++ ui.js
@@ -3,6 +3,7 @@ class UI {
         this.profile = document.getElementById('profile');
     }
 
+    // Display profile in UI
     showProfile(user) {
         this.profile.innerHTML = `
             <div class="card card-body mb-3">
@@ -11,23 +12,57 @@ class UI {
                         <img class="img-fluid mb-2" src="${user.avatar_url}">
                         <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
                     </div>
-                <div class="col-md-9">
-                    <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
-                    <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
-                    <span class="badge badge-success">Followers: ${user.followers}</span>
-                    <span class="badge badge-info">Following: ${user.following}</span>
-                    <br><br>
-                    <ul class="list-group">
-                    <li class="list-group-item">Company: ${user.company}</li>
-                    <li class="list-group-item">Website/Blog: <a href="https://${user.blog}" target="_blank">${user.blog}</a></li>
-                    <li class="list-group-item">Location: ${user.location}</li>
-                    <li class="list-group-item">Member Since: ${user.created_at}</li>
-                    </ul>
+                    <div class="col-md-9">
+                        <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
+                        <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
+                        <span class="badge badge-success">Followers: ${user.followers}</span>
+                        <span class="badge badge-info">Following: ${user.following}</span>
+                        <br><br>
+                        <ul class="list-group">
+                        <li class="list-group-item">Company: ${user.company}</li>
+                        <li class="list-group-item">Website/Blog: <a href="https://${user.blog}" target="_blank">${user.blog}</a></li>
+                        <li class="list-group-item">Location: ${user.location}</li>
+                        <li class="list-group-item">Member Since: ${user.created_at}</li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
             </div>
             <h3 class="page-heading mb-3">Latest Repos</h3>
             <div id="repos"></div>
         `;
+    }
+
+    // Show alert message
+    showAlert(message, className) {
+        // Clear any remaining alerts
+        this.clearAlert();
+        // Create div
+        const div = document.createElement('div');
+        // Add classes
+        div.className = className;
+        // Add text
+        div.appendChild(document.createTextNode(message));
+        // Get parent
+        const container = document.querySelector('.searchContainer');
+        // Get search box
+        const search = document.querySelector('.search');
+        // Insert alert
+        container.insertBefore(div, search);
+
+        // Timeout after 3 sec
+        setTimeout(() => this.clearAlert(), 3000)
+    }
+
+    // Clear alert message
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert');;
+        if(currentAlert) {
+            currentAlert.remove();
+        }
+    }
+
+    // Clear profile
+    clearProfile() {
+        this.profile.innerHTML = '';
     }
 }
